1a8587896ddaa6432d425041913f9f687c4c27c3
dineshmalik99 <dineshmalik99@gmail.com>
2018-03-26 12:31:50 +0530

    Testing around advice execution

--- src/main/java/com/example/aspect/DBConnectionAspect.java
+++ src/main/java/com/example/aspect/DBConnectionAspect.java
@@ -1,20 +1,39 @@
 package com.example.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class DBConnectionAspect {
+	
+	@Pointcut("execution(* *.getConnection(..))")
+	private void myCustomPointCut(){
+		
+	}
 
-	@Before("execution(* *.getConnection())")
-	public void beforeAdvice(){
+	@Before("myCustomPointCut()")
+	private void beforeAdvice(){
 		System.out.println("Running before Advice : DBConnectionAspect");
 	}
-	@After("execution(* *.getConnection())")
-	public void afterAdvice(){
+	@After("myCustomPointCut()")
+	private void afterAdvice(){
 		System.out.println("Running after Advice : DBConnectionAspect");
+	}
+	
+	@Around("myCustomPointCut()")
+	private void aroundAdvice(ProceedingJoinPoint point){
+		System.out.println("In Around Advice before method Execution");
+		try {
+			point.proceed();
+		} catch (Throwable e) {
+			System.out.println("Exception in procedding");
+		}
+		System.out.println("In Around Advice after method Execution");
 	}
 }
